8bf234ff40ca750c66761bdeed280fb46c55e930
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-07-04 11:14:49 -0400

    Start quiz page

---
+++ Assets/js/script.js
@@ -0,0 +1,8 @@
+const questionsEl = document.querySelector(".questions");
+const buttonEl = document.querySelector("#submit-quiz");
+
+start_quiz () {
+    
+}
+
+buttonEl.addEventListener("submit", start_quiz());
